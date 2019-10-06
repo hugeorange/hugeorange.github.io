d0b127a58dd43df192398857ee83f432beafed82
orange <326242499@qq.com>
2019-10-06 16:34:34 +0800

    样式调整&关于

---
+++ themes/icarus/includes/generators/about.js
@@ -0,0 +1,14 @@
+/**
+ * Tag list page generator
+ */
+module.exports = function (hexo) {
+    hexo.extend.generator.register('about', function (locals) {
+        return {
+            path: 'about/',
+            layout: ['about'],
+            data: Object.assign({}, locals, {
+                __about: true
+            })
+        };
+    });
+}
